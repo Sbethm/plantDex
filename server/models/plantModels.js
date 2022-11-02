bc30a19e4201a1cdf091b91ff9499d96801b45bb
Sarah <14sbethm@gmail.com>
2022-11-02 16:49:54 -0500

    Update plantModels.js

--- server/models/plantModels.js
+++ server/models/plantModels.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://Smoose:c0ding15fun!!!@cluster0.z8aried.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://***********@cluster0.z8aried.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
