9af9b29fea5f15526bcf7e6b594439105d9350ad
Sarah <14sbethm@gmail.com>
2023-01-14 20:53:36 -0600

    new plant form layout and post fetch functionality

--- src/components/MainPage/NewPlant.jsx
+++ src/components/MainPage/NewPlant.jsx
@@ -1,45 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 export default function NewPlant(props) {
+    const [ newPlant, setNewPlant ] = useState({
+        nickname: '',
+        water: '',
+        adoptday: '',
+        type: ''
+    });
 
-    const onSubmit = () => {
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setNewPlant((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
 
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log("THIS IS YOUR EVENT", event);
+        console.log("THIS IS YOUR NICKNAME", newPlant);
+
+        fetch('/plant', {
+            method: 'post',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({
+                nickname: newPlant.nickname,
+                water: newPlant.water,
+                adoptday: newPlant.adoptday,
+                type: newPlant.type            
+            })
+        }).catch(err => {
+            console.log("YOUR POST WAS NOT SUCCESSFUL", err);
+        })
     }
 
     return (
         <div className='newPlant--container'>
             <button onClick={ props.setShowModal }>X</button>
-            {/* <form action="" onClick={ onSubmit }>
+            <form action="" onSubmit={ onSubmit }>
                 <div className='plantCard--header'>
-                    <div className='title-water'>
-                        <label htmlFor="nickname">Nickname:</label>
-                        <input type="text" name="nickname" />
-                        <label htmlFor="water"> How much water does it drink?</label>
-                        <select name="water" id="water-values">
-                            <option value="dry">
-                                <FontAwesomeIcon className='plantCard--image' icon="droplet" />
-                                Dry
-                            </option>
-                            <option value="Moist">
-                                <FontAwesomeIcon className='plantCard--image' icon="glassWater" />
-                                Moist
-                            </option>
-                            <option value="Wet">
-                                <FontAwesomeIcon className='plantCard--image' icon="cloudShowersWater" />
-                                Wet
-                            </option>
-                        </select>
+                    <div className='nickname-water--input'>
+                        <label>
+                            Nickname:
+                            <input 
+                              type='text'
+                              placeholder='Prickly Pete' 
+                              name='nickname'
+                              value={newPlant.nickname}
+                              onChange={handleChange}
+                            />
+                        </label>
+                        <label> 
+                            How much water does it drink?
+                            <select 
+                              name="water" 
+                              value={newPlant.water}
+                              onChange={handleChange}
+                              id="water-values"
+                            >
+                                <option value="dry">
+                                    Dry
+                                </option>
+                                <option value="Moist">
+                                    Moist
+                                </option>
+                                <option value="Wet">
+                                    Wet
+                                </option>
+                            </select>
+                        </label>
+                        <label>
+                            "Got It" Day:
+                            <input 
+                              type="date"  
+                              name="adoptday" 
+                              value={newPlant.adoptday}
+                              onChange={handleChange}
+                            />
+                        </label>
                     </div>
                     <p className='plant--detail'>{  }</p>
                 </div>
                 <div className='plantCard--details'>          
                     <FontAwesomeIcon className='plantCard--image' icon="seedling" />
-                    <label htmlFor="type">Type:</label>
-                    <input type="text" name="type" />
+                    <label>
+                        Type:
+                        <input 
+                          type="text" 
+                          placeholder='cactus' 
+                          name='type'
+                          value={newPlant.type}
+                          onChange={handleChange}
+                        />
+                    </label>
+                    
                 </div>
-            </form> */}
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
